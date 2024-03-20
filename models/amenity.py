2be683962dbe7af1de9bbf411de10caa315205b8
Jojo-Programming <mpagijollyuganda@gmail.com>
2024-03-20 15:56:10 -0700

    going solo

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+
+""" The State Module for HBNB project """
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """The amenity class, contains name field and places relationship.
+    """
+    __tablename__ = "amenities"
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column('name', String(128), nullable=False)
+        place_amenity = relationship('Place',
+                                     secondary='place_amenity',
+                                     back_populates='amenities')
+    else:
+        name = ""
--- models/user.py
+++ models/user.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
+
 """This module defines a class User"""
-from models.base_model import BaseModel
+
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        email = Column("email", String(128), nullable=False)
+        password = Column("password", String(128), nullable=False)
+        first_name = Column("first_name", String(128))
+        last_name = Column("last_name", String(128))
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
